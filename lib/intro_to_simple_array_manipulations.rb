421a484fd668a61ab6a55d4652a5ec333351ad7f
Edward Castillo <castilloedwarda@gmail.com>
2019-10-24 03:37:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,30 +5,39 @@ end
 def using_unshift (array, string)
   array.unshift string
 end
+
 def using_pop (array)
   array.pop
 end
+
 def pop_with_args (array)
   array.pop(2)
 end
+
 def using_shift (array, array)
   array.shift 
 end
+
 def shift_with_args(array)
   array.shift(2)
 end
+
 def using_concat (array, array2)
   array.concact(array2)
 end
+
 def using_uniq (array)
   array.using_uniq
 end
+
 def using_flatten (array)
   array.flatten 
 end
+
 def using_delete (array, string)
   array.delete
 end
+
 def using_delete_at (array, integer)
   array.delete(integer)
 end
